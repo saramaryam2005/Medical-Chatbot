b6b7558bb40429142b87dcfe99611740fdfcfbe2
Sara Maryam <227032307+saramaryam2005@users.noreply.github.com>
2026-06-28 21:25:55 +0530

    update

--- app.py
+++ app.py
@@ -9,3 +9,18 @@ def home():
     return render_template("index.html")
 
 
+@app.route("/get", methods=["POST"])
+def chat():
+
+    user_message = request.form["msg"]
+
+    result = get_response(user_message)
+
+    return jsonify({
+    "answer": result["answer"],
+    "sources": result["sources"]
+})
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
